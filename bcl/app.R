ec4040c81646368fba02aeca02baa81e1166f590
Frederike Basedow <f.basedow@arcor.de>
2018-11-21 15:07:18 -0800

    add changes to bcl app

--- bcl/app.R
+++ bcl/app.R
@@ -1,56 +1,96 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(DT)
 
-bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+bcl <- read.csv("/Users/frederikebasedow/Documents/uni/UBC Neuroscience/courses/STAT547/STAT547-HW08-repository/bcl/bcl-data.csv", stringsAsFactors = FALSE)
 
 ui <- fluidPage(
-  titlePanel("BC Liquor Store prices"),
+  titlePanel("BC Liquor Store Inventory"),
   sidebarLayout(
     sidebarPanel(
+      h5("Wondering what drink to get for tonight? This app will help you through the selection of the BC Liquor Store."),
       sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
-      radioButtons("typeInput", "Product type",
-                  choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
-                  selected = "WINE"),
-      uiOutput("countryOutput")
+      uiOutput("typeOutput"),
+      checkboxInput("countryFilter", "Filter by country", value = FALSE),
+      conditionalPanel(
+        condition = "input.countryFilter",
+        uiOutput("countryOutput")
+      )
     ),
+
     mainPanel(
+      h4(textOutput("summary")),
+      br(), br(),
       plotOutput("coolplot"),
       br(), br(),
-      tableOutput("results")
+      DT::dataTableOutput("results")
     )
   )
 )
 
 server <- function(input, output) {
+  output$typeOutput <- renderUI({
+    selectInput("typeInput", "Type of drink",
+      sort(unique(bcl$Type)),
+      multiple = TRUE,
+      selected = c("BEER", "WINE")
+    )
+  })
+
+  output$subtypeOutput <- renderUI({
+    selectInput("subtypeInput", "Type of wine",
+      choices = c("RED", "WHITE"),
+      multiple = TRUE,
+      selected = "RED"
+    )
+  })
+
   output$countryOutput <- renderUI({
     selectInput("countryInput", "Country",
-                sort(unique(bcl$Country)),
-                selected = "CANADA")
-  })  
-  
+      sort(unique(bcl$Country)),
+      selected = "CANADA"
+    )
+  })
+
   filtered <- reactive({
     if (is.null(input$countryInput)) {
       return(NULL)
-    }    
-    
-    bcl %>%
-      filter(Price >= input$priceInput[1],
-             Price <= input$priceInput[2],
-             Type == input$typeInput,
-             Country == input$countryInput
-      )
-  })
-  
-  output$coolplot <- renderPlot({
-    if (is.null(filtered())) {
-      return()
     }
-    ggplot(filtered(), aes(Alcohol_Content)) +
-      geom_histogram()
+    if (input$countryFilter) {
+      bcl %>% filter(
+        Country == input$countryInput,
+        Price >= input$priceInput[1],
+        Price <= input$priceInput[2],
+        Type %in% input$typeInput
+      )
+    }
+    else {
+      bcl %>%
+        filter(
+          Price >= input$priceInput[1],
+          Price <= input$priceInput[2],
+          Type %in% input$typeInput
+        )
+    }
   })
 
-  output$results <- renderTable({
+  output$summary <- renderText({
+    nDrinks <- nrow(filtered())
+    paste0("We found ", nDrinks, " drinks for you")
+  })
+
+  output$coolplot <- renderPlot({
+    if (is.null(filtered())) {
+      return(NULL)
+    }
+    ggplot(filtered(), aes(Alcohol_Content, fill = Type)) +
+      geom_histogram(colour = "black") +
+      labs(x = "Alcohol content", y = "Number of beverages") +
+      theme_bw()
+  })
+
+  output$results <- DT::renderDataTable({
     filtered()
   })
 }
